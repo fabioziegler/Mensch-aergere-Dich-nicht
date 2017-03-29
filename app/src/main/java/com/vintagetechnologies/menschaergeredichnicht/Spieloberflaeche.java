2189439322bc871b93757bb6fb9294e8cbdc9595
DemiMarieD <demi-marie@outlook.de>
2017-03-29 20:37:45 +0200

    erstes Interface für Spieloberfläche  (schwarz-weiß) und bugfixed

---
+++ app/src/main/java/com/vintagetechnologies/menschaergeredichnicht/Spieloberflaeche.java
@@ -0,0 +1,13 @@
+package com.vintagetechnologies.menschaergeredichnicht;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spieloberflaeche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spieloberflaeche);
+    }
+}
