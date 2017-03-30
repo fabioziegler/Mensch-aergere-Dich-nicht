4af04682a805a6a1bfa68da0773c4598ce4cc9f8
DemiMarieD <demi-marie@outlook.de>
2017-03-30 09:17:39 +0200

    erste Button impl. und Inteface für Spielregeln wurde erstellt

--- app/src/main/java/com/vintagetechnologies/menschaergeredichnicht/MainActivity.java
+++ app/src/main/java/com/vintagetechnologies/menschaergeredichnicht/MainActivity.java
@@ -1,13 +1,32 @@
 package com.vintagetechnologies.menschaergeredichnicht;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText)(findViewById(R.id.editText_name));
+        btnGo = (Button)(findViewById(R.id.BTN_go_hauptmenue));
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // toDO: Einfügen der Bedingung "nur wenn name != null" sonst Fehlermeldung
+                startActivity(new Intent(MainActivity.this, Hauptmenue.class));
+            }
+        });
+
     }
 }
