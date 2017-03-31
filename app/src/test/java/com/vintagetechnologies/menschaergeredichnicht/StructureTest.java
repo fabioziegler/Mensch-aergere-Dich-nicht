ce78cf88c50938a28e5f64e74fd5c2cfa175a92c
Johannes Hölzl <johannes.hoelzl@gmail.com>
2017-03-31 22:34:44 +0200

    Struktur hinzugefügt

---
+++ app/src/test/java/com/vintagetechnologies/menschaergeredichnicht/StructureTest.java
@@ -0,0 +1,14 @@
+package com.vintagetechnologies.menschaergeredichnicht;
+
+import org.junit.Test;
+
+/**
+ * Created by johannesholzl on 31.03.17.
+ */
+
+public class StructureTest {
+    @Test
+    public void test1(){
+        //folgen noch
+    }
+}
