3d479ecf51e0ced69ef83c04702b0c17a6d67535
ratrepotec <ratrepotec@edu.aau.at>
2017-04-03 14:41:45 +0200

    Würfel

---
+++ app/src/main/java/com/vintagetechnologies/menschaergeredichnicht/structure/DiceNumber.java
@@ -0,0 +1,21 @@
+package com.vintagetechnologies.menschaergeredichnicht.structure;
+
+/**
+ * Created by Rainer on 03.04.2017.
+ */
+
+public enum DiceNumber {
+
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6);
+
+    private int number;
+
+    DiceNumber(int n) {
+
+        this.number = n;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+}
