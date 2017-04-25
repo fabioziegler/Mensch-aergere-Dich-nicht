92f42a8bcf9cad17ff7d984f2228962e6441968a
Simon <simonwegmeyer@yahoo.de>
2017-04-25 13:16:20 +0200

    Spiel Synchronisierung Prototyp v1

---
+++ app/src/main/java/com/vintagetechnologies/menschaergeredichnicht/synchronisation/GameSynchronisation.java
@@ -0,0 +1,30 @@
+package com.vintagetechnologies.menschaergeredichnicht.synchronisation;
+
+import com.google.gson.Gson;
+import com.vintagetechnologies.menschaergeredichnicht.GameLogic;
+import com.vintagetechnologies.menschaergeredichnicht.structure.Game;
+import com.vintagetechnologies.menschaergeredichnicht.structure.Player;
+
+/**
+ * Created by Simon on 25.04.2017.
+ */
+
+public class GameSynchronisation {
+
+    public static void synchronize(GameLogic gameLogic, Game game){
+
+    }
+
+    private static String encode(Game game){
+
+    }
+
+    private static Game decode(String fromJson){
+
+    }
+
+    private static void sendToOtherDevices(){
+
+    }
+
+}
