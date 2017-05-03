33105b61f3231ba77e1c94be0cad32dff06025c9
ratrepotec <ratrepotec@edu.aau.at>
2017-05-03 14:37:07 +0200

    DiceTest angelegt

---
+++ app/src/test/java/com/vintagetechnologies/menschaergeredichnicht/DiceTest.java
@@ -0,0 +1,26 @@
+package com.vintagetechnologies.menschaergeredichnicht;
+
+import com.vintagetechnologies.menschaergeredichnicht.structure.Dice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Rainer on 03.05.2017.
+ */
+
+public class DiceTest {
+
+    Dice dice;
+
+    @Before
+    public void beforeTest(){
+        dice = new Dice();
+    }
+
+    @Test
+    public void testBlacklist(){
+        dice.
+    }
+
+}
