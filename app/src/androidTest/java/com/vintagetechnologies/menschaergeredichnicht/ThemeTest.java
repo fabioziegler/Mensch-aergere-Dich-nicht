98edc2133d48633abeca6e141c257d6533e48745
Johannes Hölzl <johannes.hoelzl@gmail.com>
2017-05-15 21:11:10 +0200

    Theme Test

---
+++ app/src/androidTest/java/com/vintagetechnologies/menschaergeredichnicht/ThemeTest.java
@@ -0,0 +1,63 @@
+package com.vintagetechnologies.menschaergeredichnicht;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.test.runner.AndroidJUnit4;
+
+
+import com.vintagetechnologies.menschaergeredichnicht.structure.Theme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by johannesholzl on 10.05.17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ThemeTest {
+
+
+    @Test
+    public void testTheme() {
+
+
+        AssetManager am = getInstrumentation().getContext().getAssets();
+
+
+
+        try {
+            String s [] = am.list("/");
+            for(String i : s){
+                System.out.println("1 "+i);
+            }
+
+            String a [] = am.list("themes");
+            for(String i : a){
+                System.out.println("2 "+i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        Theme t = null;
+        try {
+            t = new Theme(am.open("themes/classic.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+
+        assertEquals((int) Color.rgb(255, 0, 0), (int) t.getColor("RED"));
+    }
+}
